78bf5fe78a6e474f8de818d920ea6cce36fd38b1
facelessloser <facelessloser@debianThinkpad.facelessloser>
2021-10-17 15:42:59 +0100

    changed the button pin to 26

--- night_shutdown/pishutdown.py
+++ night_shutdown/pishutdown.py
@@ -1,19 +1,18 @@
-# Button connected to pin 31
+# Button connected to pin 26 
 
 import RPi.GPIO as GPIO
 from subprocess import call
 import time
 
-
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def buttonStateChanged(pin):
 
     if not (GPIO.input(pin)):
         call(['shutdown', '-h', 'now'], shell=False)
 
-GPIO.add_event_detect(31, GPIO.BOTH, callback=buttonStateChanged)
+GPIO.add_event_detect(26, GPIO.BOTH, callback=buttonStateChanged)
 
 while True:
     # sleep to reduce unnecessary CPU usage
